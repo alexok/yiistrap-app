4c58ab6aa146c8cef9869a9d861bdbc8a4f6b49f
alexok <okkel@kontur-agency.ru>
2013-09-24 14:38:40 +0700

    Initial commit

---
+++ protected/components/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: Alexok
+ * Date: 09.09.13
+ * Time: 16:31
+ */
+
+class AdminController extends BaseController
+{
+
+}
---
+++ protected/components/BaseController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: Alexok
+ * Date: 09.09.13
+ * Time: 16:30
+ */
+
+class BaseController extends CController
+{
+
+}
